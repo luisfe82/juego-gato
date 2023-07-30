9529709d326de49d918434f852173802c0d90459
Luisfe82 <luisfediana@gmail.com>
2023-07-30 09:58:12 -0600

    actualizado

---
+++ proyecto/js/01.console.log.js
@@ -0,0 +1,8 @@
+// ! console.log como funciona
+console.log("Hola Mundo");
+console.info("Codigo incorrecto");
+console.warn("Peligro");
+console.error("Error");
+// console.clear();
+
+console.log("%c ¿Hola! ","background:linear-gradient(#000,#555);color:#fff;padding: 5px 10px;");
